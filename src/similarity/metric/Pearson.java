10e2621026e24fa9ac9894adee5a8e27493b94c9
Derek Organ <derek@1timetracking.com>
2012-02-07 17:09:30 +0000

    Updated the pearson algorightm to be more efficient. Still gets the same value.

--- src/similarity/metric/Pearson.java
+++ src/similarity/metric/Pearson.java
@@ -30,6 +30,8 @@ public class Pearson implements SimilarityMetric
 	{
        
 		double top = 0;
+		double base1 = 0;
+		double base2 = 0;
 		
 		Set<Integer> common = p1.getCommonIds(p2);
 		for(Integer id: common)
@@ -37,20 +39,10 @@ public class Pearson implements SimilarityMetric
 			double r1 = p1.getValue(id).doubleValue() - p1.getMeanValue();
 			double r2 = p2.getValue(id).doubleValue() - p2.getMeanValue();
 			top += r1 * r2;
+			base1 += Math.pow(r1, 2);
+			base2 += Math.pow(r2, 2);
 		}
 		
-		double base1 = 0;
-		double base2 = 0;
-	
-		for(Integer id: common)
-		{
-			double n1 = p1.getValue(id).doubleValue() - p1.getMeanValue();
-			double n2 = p2.getValue(id).doubleValue() - p2.getMeanValue();
-			
-			base1 += Math.pow(n1, 2);
-			base2 += Math.pow(n2, 2);
-		}
-	
 		return (base1 > 0 && base2 > 0) ? top / (Math.sqrt(base1) * Math.sqrt(base2)) : 0;
 	}
 	
